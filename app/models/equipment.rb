e0154a892076dc6a961cbd1e08f16ca6b6de8281
Carl Svensson <csvensson@apple.com>
2025-01-20 20:48:39 -0800

    Fixup donation views

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -3,6 +3,6 @@ class Equipment < ApplicationRecord
   belongs_to :person, ->{ includes(:instrument) }, required: false
 
   def to_html
-    return "<strong>#{instrument&.name}</strong> #{model} #{note ? '(' + note + ')' : ''}"
+    return "<strong>#{instrument&.kind&.titleize}</strong> #{instrument&.model} #{note ? '(' + note + ')' : ''}"
   end
 end
