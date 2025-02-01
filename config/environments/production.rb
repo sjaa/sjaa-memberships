40b678e007b32e9301643ea43b0e0b6f2c797358
Carl Svensson <csvensson@apple.com>
2025-01-31 20:44:24 -0800

    Mailtrap settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,11 +79,11 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              ENV['SMTP_ADDRESS'],
     port:                 ENV['SMTP_PORT'],
-    domain:               ENV['SMTP_DOMAIN'],
+    #domain:               ENV['SMTP_DOMAIN'],
     user_name:            ENV['SMTP_USER_NAME'],
     password:             ENV['SMTP_PASSWORD'],
-    authentication:       'plain',
-    enable_starttls_auto: true
+    authentication:       :login,
+    #enable_starttls_auto: true
   }
 
   # Ignore bad email addresses and do not raise email delivery errors.
