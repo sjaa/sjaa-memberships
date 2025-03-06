84cf8025c9e4c15d7b1650296243c72230475fe1
Carl Svensson <csvensson@apple.com>
2025-03-05 22:14:40 -0800

    eh stuff

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,11 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    include Authenticatable
+    identified_by :current_user
+
+    def connect
+      authenticate!
+      self.current_user = @current_user # maybe redundant
+    end
   end
 end
