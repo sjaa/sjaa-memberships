b9a23186c11e3d298d822b2c4cd6be16e6f6f82c
Carl Svensson <csvensson@apple.com>
2025-03-08 20:18:06 -0800

    Ephemeris report update

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -6,8 +6,29 @@ class ReportsController < ApplicationController
     filter({ephemeris: 'printed'})
 
     if(params[:page])
-      render turbo_stream: turbo_stream.replace('people', partial: 'people/index')
+      render turbo_stream: turbo_stream.replace('people', partial: 'people/ephemeris_index')
       return
+    end
+
+    respond_to do |format|
+      format.html
+      format.csv do
+        csv_data = CSV.generate(headers: true) do |csv|
+          csv << ['First Name', 'Last Name', 'Address', 'City', 'State', 'Zip']
+          @all_people.each do |person|
+            csv << [
+            person.first_name,
+            person.last_name,
+            person.primary_contact&.address,
+            person.primary_contact&.city&.name,
+            person.primary_contact&.state&.name,
+            person.primary_contact&.zipcode,
+            ]
+          end
+        end
+
+        send_data csv_data, filename: "sjaa-ephemeris-#{Date.today}.csv"
+      end
     end
   end
 end
