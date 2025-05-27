406d8a39f92f3c162cddee5531416dbb1f10d59b
Carl Svensson <csvensson@apple.com>
2025-05-26 22:59:44 -0700

    scheduled renewals

--- app/jobs/renewal_reminders_job.rb
+++ app/jobs/renewal_reminders_job.rb
@@ -1,18 +1,38 @@
 class RenewalRemindersJob < ApplicationJob
   queue_as :default
-
-  def perform_test_thingy(*args)
-    AccountMailer.sample.deliver_now
-  end
-
+  
   def perform(*args)
-    # Iterate over renewable members
-    Person.renewable_members.each do |person|
-      # Send individual reminders
-      AccountMailer.renewal_notice(person).deliver_now
-
-      # Self-imposed rate-limiting
-      sleep(1)
+    enable = args[0].downcase.strip == 'enable'
+    schedule = args[1]
+    
+    if(enable && schedule_match(schedule))
+      # Iterate over renewable members
+      Person.renewable_members.each do |person|
+        # Send individual reminders
+        AccountMailer.renewal_notice(person).deliver_now
+        
+        # Self-imposed rate-limiting
+        sleep(1)
+      end
     end
+    
+  end # perform
+  
+  # Decode a simplified cron-like schedule
+  # Month Day Hour
+  def schedule_match(schedule)
+    today = DateTime.now
+    schedule_mdh = schedule.split(' ')
+    today_mdh = [today.month, today.day, today.hour]
+    comparison = true
+    
+    today_mdh.each_with_index do |unit, index|
+      sched = schedule_mdh[index]
+      #puts "sched: #{sched} index: #{index} unit: #{unit} comparison: #{comparison}"
+      comparison = comparison && (sched.nil? || sched == '*' || sched.to_i == unit)
+    end
+    
+    return comparison
   end
+  
 end
