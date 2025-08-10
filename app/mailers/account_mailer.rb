205b85fd42443e011c87763fc513d7a210f91d48
Carl Svensson <csvensson@apple.com>
2025-08-10 16:04:10 -0700

    Add invoice to welcome email

--- app/mailers/account_mailer.rb
+++ app/mailers/account_mailer.rb
@@ -27,6 +27,9 @@ class AccountMailer < ApplicationMailer
 
   def welcome(membership)
     @membership = membership
+    @order = membership&.order
+    @order_total = @order&.price || 0
+    @order_donation = @order&.membership_params&.dig(:donation_amount).to_f
     @person = membership.person
     @renewal = @person.memberships.count > 1
     mail(to: @person.email, bcc: %w(officers@sjaa.net memberships@sjaa.net donations@sjaa.net), subject: "SJAA Membership - Welcome and Thank You! (#{@person&.last_name}, #{@person&.first_name})")
