560abf413dadfa9114ce1562399fabd1b0f608a2
cecomp64 <csvenss2@gmail.com>
2025-08-12 22:09:53 -0700

    Possible fix for getting live paypal mode

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,6 @@ module SjaaMembers
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
-    config.paypal_mode = :sandbox # Valid values are :sandbox and :live.
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
