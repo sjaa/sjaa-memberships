4a35007897570f191d759803d1a3dcd5c3b98918
cecomp64 <csvenss2@gmail.com>
2025-09-05 22:43:49 -0700

    Fix Test support

--- config/initializers/debug.rb
+++ config/initializers/debug.rb
@@ -1,5 +1,9 @@
 # Debug configuration for Docker remote debugging
-if Rails.env.development? && ENV['RUBY_DEBUG_OPEN']
+# Note: Tests use docker compose run with RUBY_DEBUG_OPEN= to disable this
+if Rails.env.development? && 
+   ENV['RUBY_DEBUG_OPEN'] && 
+   !ENV['RUBY_DEBUG_OPEN'].empty? &&
+   ENV['RUBY_DEBUG_OPEN'] != 'false'
   puts "🐛 Starting debug server on #{ENV['RUBY_DEBUG_HOST']}:#{ENV['RUBY_DEBUG_PORT']}"
   require 'debug/open_nonstop'
 end
