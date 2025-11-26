7805989208aa53ec22ee84fb18e23a3016c92c46
cecomp64 <csvenss2@gmail.com>
2025-11-25 23:42:50 -0800

    Allow cross origin requests for widgets

--- app/controllers/widget_controller.rb
+++ app/controllers/widget_controller.rb
@@ -3,8 +3,19 @@ class WidgetController < ApplicationController
 
   # Skip authentication for public widgets
   skip_before_action :authenticate!, only: [:meetup]
+  before_action :allow_cross_origin, if: :embeddable?
+
+  def allow_cross_origin
+    headers['Access-Control-Allow-Origin'] = '*'
+  end
+
+  def embeddable?
+    request.headers['X-Frame-Options'].present?
+  end
 
   def meetup
+    response.headers['Content-Type'] = 'text/html'
+    response.headers['X-Frame-Options'] = 'ALLOW-FROM https://sites.google.com'
     @events = MeetupEvent.upcoming.limit(10)
   end
 end
