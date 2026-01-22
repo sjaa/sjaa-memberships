5a02d1df27e7a5d4c4728325b57c94428c2456c5
Carl Svensson <csvenss2@gmail.com>
2026-01-21 23:04:52 -0800

    forget the prefix already

--- config/initializers/active_storage.rb
+++ config/initializers/active_storage.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 # Add a prefix to all ActiveStorage blob keys for easy identification and retrieval
-Rails.application.config.to_prepare do
-  ActiveStorage::Blob.class_eval do
-    before_create :add_blob_key_prefix
-
-    private
-
-    def add_blob_key_prefix
-      self.key = "sjaa-memberships-#{Rails.env}/#{self.class.generate_unique_secure_token}"
-    end
-  end
-end
+#Rails.application.config.to_prepare do
+#  ActiveStorage::Blob.class_eval do
+#    before_create :add_blob_key_prefix
+#
+#    private
+#
+#    def add_blob_key_prefix
+#      self.key = "sjaa-memberships-#{Rails.env}/#{self.class.generate_unique_secure_token}"
+#    end
+#  end
+#end
